e87c0121f29bc45e6ec63026caccdbbc6d4bafc6
chrislapidas <clapidas@gmail.com>
2017-07-06 16:38:38 -0400

    Added custom JS for sidebar icons

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+$("#profilepic").hover(function(){
+  $(this).css("opacity", ".7");},
+  function(){
+  $(this).css("opacity", "1");
+});
+$(".sidebar-icon").hover(function(){
+  $(this).css("opacity", ".7");},
+  function(){
+  $(this).css("opacity", "1");
+});
